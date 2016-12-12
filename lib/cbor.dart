7400d7941319aa380c5a12d7c969052afe547e35
steve <steve.hamblett@linux.com>
2016-12-12 12:02:53 +0000

    Update base files

--- lib/cbor.dart
+++ lib/cbor.dart
@@ -1,5 +1,4 @@
-// Copyright (c) 2016, steve. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+// Copyright (c) 2016, Steve Hamblett. All rights reserved.
 
 /// Support for doing something awesome.
 ///
--- lib/src/cbor_base.dart
+++ lib/src/cbor_base.dart
@@ -1,6 +1,4 @@
-// Copyright (c) 2016, steve. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
+// Copyright (c) 2016, Steve Hamblett. All rights reserved.
 // TODO: Put public facing types in this file.
 
 /// Checks if you are awesome. Spoiler: you are.
