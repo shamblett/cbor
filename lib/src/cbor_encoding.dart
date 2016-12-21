d645a869ffecbb6b97b145f35544878a3635492d
steve <steve.hamblett@linux.com>
2016-12-21 15:40:42 +0000

    Encoding - partial

--- lib/src/cbor_encoding.dart
+++ lib/src/cbor_encoding.dart
@@ -18,16 +18,17 @@ const int uint16Max = 2 ^ 16 - 1;
 const int uint32Max = 2 ^ 32 - 1;
 const int uint64Max = 2 ^ 64 - 1;
 
-int cborEncodeUint8(int value, ByteData buffer, int bufferSize) {
+int cborEncodeUint8(int value, ByteData buffer, int bufferSize,
+    [int offset = 0]) {
   assert(value >= 0 && value <= uint8Max);
   if (value <= 23) {
     if (bufferSize >= 1) {
-      buffer.setUint8(0, value);
+      buffer.setUint8(0, value + offset);
       return 1;
     }
   } else {
     if (bufferSize >= 2) {
-      buffer.setUint8(0, 0x18);
+      buffer.setUint8(0, 0x18 + offset);
       buffer.setUint8(1, value);
       return 2;
     }
@@ -35,10 +36,11 @@ int cborEncodeUint8(int value, ByteData buffer, int bufferSize) {
   return 0;
 }
 
-int cborEncodeUint16(int value, ByteData buffer, int bufferSize) {
+int cborEncodeUint16(int value, ByteData buffer, int bufferSize,
+    [int offset = 0]) {
   assert(value >= 0 && value <= uint16Max);
   if (bufferSize >= 3) {
-    buffer.setUint8(0, 0x19);
+    buffer.setUint8(0, 0x19 + offset);
     buffer.setUint16(1, value);
     return 3;
   } else {
@@ -46,10 +48,11 @@ int cborEncodeUint16(int value, ByteData buffer, int bufferSize) {
   }
 }
 
-int cborEncodeUint32(int value, ByteData buffer, int bufferSize) {
+int cborEncodeUint32(int value, ByteData buffer, int bufferSize,
+    [int offset = 0]) {
   assert(value >= 0 && value <= uint32Max);
   if (bufferSize >= 5) {
-    buffer.setUint8(0, 0x1A);
+    buffer.setUint8(0, 0x1A + offset);
     buffer.setUint32(1, value);
     return 5;
   } else {
@@ -57,10 +60,11 @@ int cborEncodeUint32(int value, ByteData buffer, int bufferSize) {
   }
 }
 
-int cborEncodeUint64(int value, ByteData buffer, int bufferSize) {
+int cborEncodeUint64(int value, ByteData buffer, int bufferSize,
+    [int offset = 0]) {
   assert(value >= 0 && value <= uint64Max);
   if (bufferSize >= 9) {
-    buffer.setUint8(0, 0x1B);
+    buffer.setUint8(0, 0x1B + offset);
     buffer.setUint64(1, value);
     return 9;
   } else {
@@ -68,66 +72,33 @@ int cborEncodeUint64(int value, ByteData buffer, int bufferSize) {
   }
 }
 
-int cborEncodeUint(int value, ByteData buffer, int bufferSize) {
+int cborEncodeUint(int value, ByteData buffer, int bufferSize,
+    [int offset = 0]) {
   if (value <= uint16Max)
     if (value <= uint8Max)
-      return cborEncodeUint8(value, buffer, bufferSize);
+      return cborEncodeUint8(value, buffer, bufferSize, offset);
     else
-      return cborEncodeUint16(value, buffer, bufferSize);
+      return cborEncodeUint16(value, buffer, bufferSize, offset);
   else if (value <= uint32Max)
-    return cborEncodeUint32(value, buffer, bufferSize);
+    return cborEncodeUint32(value, buffer, bufferSize, offset);
   else
-    return cborEncodeUint64(value, buffer, bufferSize);
+    return cborEncodeUint64(value, buffer, bufferSize, offset);
 }
 
 int cborEncodeNegint8(int value, ByteData buffer, int bufferSize) {
-  assert(value >= 0 && value <= uint8Max);
-  if (value <= 23) {
-    if (bufferSize >= 1) {
-      buffer.setUint8(0, value + 0x20);
-      return 1;
-    }
-  } else {
-    if (bufferSize >= 2) {
-      buffer.setUint8(0, 0x18 + 0x20);
-      buffer.setUint8(1, value);
-      return 2;
-    }
-  }
-  return 0;
+  return cborEncodeUint8(value, buffer, bufferSize, 0x20);
 }
 
 int cborEncodeNegint16(int value, ByteData buffer, int bufferSize) {
-  assert(value >= 0 && value <= uint16Max);
-  if (bufferSize >= 3) {
-    buffer.setUint8(0, 0x19 + 0x20);
-    buffer.setUint16(1, value);
-    return 3;
-  } else {
-    return 0;
-  }
+  return cborEncodeUint16(value, buffer, bufferSize, 0x20);
 }
 
 int cborEncodeNegint32(int value, ByteData buffer, int bufferSize) {
-  assert(value >= 0 && value <= uint32Max);
-  if (bufferSize >= 5) {
-    buffer.setUint8(0, 0x1A + 0x20);
-    buffer.setUint32(1, value);
-    return 5;
-  } else {
-    return 0;
-  }
+  return cborEncodeUint32(value, buffer, bufferSize, 0x20);
 }
 
 int cborEncodeNegint64(int value, ByteData buffer, int bufferSize) {
-  assert(value >= 0 && value <= uint64Max);
-  if (bufferSize >= 9) {
-    buffer.setUint8(0, 0x1B + 0x20);
-    buffer.setUint64(1, value);
-    return 9;
-  } else {
-    return 0;
-  }
+  return cborEncodeUint64(value, buffer, bufferSize, 0x20);
 }
 
 int cborEncodeNegint(int value, ByteData buffer, int bufferSize) {
@@ -140,4 +111,89 @@ int cborEncodeNegint(int value, ByteData buffer, int bufferSize) {
     return cborEncodeNegint32(value, buffer, bufferSize);
   else
     return cborEncodeNegint64(value, buffer, bufferSize);
+}
+
+int cborEncodeBytestringStart(int length, ByteData buffer, int bufferSize) {
+  return cborEncodeUint8(length, buffer, bufferSize, 0x40);
+}
+
+int _cborEncodeByte(int value, ByteData buffer, int bufferSize) {
+  if (bufferSize >= 1) {
+    buffer.setUint8(0, value);
+    return 1;
+  } else
+    return 0;
+}
+
+int cborEncodeIndefBytestringStart(ByteData buffer, int bufferSize) {
+  return _cborEncodeByte(0x5F, buffer, bufferSize);
+}
+
+int cborEncodeStringStart(int length, ByteData buffer, int bufferSize) {
+  return cborEncodeUint(length, buffer, bufferSize, 0x60);
+}
+
+int cborEncodeIndefStringStart(ByteData buffer, int bufferSize) {
+  return _cborEncodeByte(0x7F, buffer, bufferSize);
+}
+
+int cborEncodeArrayStart(int length, ByteData buffer, int bufferSize) {
+  return cborEncodeUint(length, buffer, bufferSize, 0x80);
+}
+
+int cborEncodeIndefArrayStart(ByteData buffer, int bufferSize) {
+  return _cborEncodeByte(0x9F, buffer, bufferSize);
+}
+
+int cborEncodeMapStart(int length, ByteData buffer, int bufferSize) {
+  return cborEncodeUint(length, buffer, bufferSize, 0xA0);
+}
+
+int cborEncodeIndefMapStart(ByteData buffer, int bufferSize) {
+  return _cborEncodeByte(0xBF, buffer, bufferSize);
+}
+
+int cborEncodeTag(int value, ByteData buffer, int bufferSize) {
+  return cborEncodeUint(value, buffer, bufferSize, 0xC0);
+}
+
+int cborEncodeBool(bool value, ByteData buffer, int bufferSize) {
+  return value
+      ? _cborEncodeByte(0xF5, buffer, bufferSize)
+      : _cborEncodeByte(0xF4, buffer, bufferSize);
+}
+
+int cborEncodeNull(ByteData buffer, int bufferSize) {
+  return _cborEncodeByte(0xF6, buffer, bufferSize);
+}
+
+int cborEncodeUndef(ByteData buffer, int bufferSize) {
+  return _cborEncodeByte(0xF7, buffer, bufferSize);
+}
+
+int cborEncodeHalf(double value, ByteData buffer, int bufferSize) {
+/* Assuming value is normalized */
+  final int val = value.toInt();
+  int res;
+  int exp =
+      (val & 0x7F800000) >> 23; /* 0b0111_1111_1000_0000_0000_0000_0000_0000 */
+  final int mant = val & 0x7FFFFF; /* 0b0000_0000_0111_1111_1111_1111_1111_1111 */
+  if (exp == 0xFF) {
+    /* Infinity or NaNs */
+    if (value != value)
+      res = 0x00e700; /* Not IEEE semantics - required by CBOR [s. 3.9] */
+    else
+      res = (val & 0x80000000) >> 16 | 0x7C00 | (mant != 0 ? 1 : 0) << 15;
+  } else if (exp == 0x00) {
+    /* Zeroes or subnorms */
+    res = (val & 0x80000000) >> 16 | (mant >> 13);
+  } else {
+    /* Normal numbers */
+    exp -= 127;
+    if (((exp) > 15 || (exp) < -14))
+      return 0; /* No way we can represent magnitude in normalized way */
+    else
+      res = (val & 0x80000000) >> 16 | ((exp + 15) << 10) | (mant >> 13);
+  }
+  return cborEncodeUint16(res, buffer, bufferSize, 0xE0);
 }
