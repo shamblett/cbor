629692fe86af590e23028cab2201fbf629e9ae97
Steve Hamblett <steve.hamblett@linux.com>
2017-02-17 11:49:19 +0000

    Split testss

---
+++ test/cbor_combined_test.dart
@@ -0,0 +1,36 @@
+/*
+ * Package : Cbor
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 12/12/2016
+ * Copyright :  S.Hamblett
+ */
+import 'package:cbor/cbor.dart' as cbor;
+import 'package:test/test.dart';
+
+void main() {
+  group('Original C++ tests', () {
+    test('Encode/Decode confidence -> ', () {
+      // Encoding
+      final cbor.OutputDynamic output = new cbor.OutputDynamic();
+      final cbor.Encoder encoder = new cbor.Encoder(output);
+      encoder.writeArray(9);
+      encoder.writeInt(123);
+      encoder.writeInt(-457);
+      encoder.writeString("barrr");
+      encoder.writeInt(321);
+      encoder.writeInt(322);
+      encoder.writeString("foo");
+      encoder.writeBool(true);
+      encoder.writeBool(false);
+      encoder.writeNull();
+      encoder.writeUndefined();
+
+      // Decoding
+      final cbor.Input input = new cbor.Input(output.getData(), output.size());
+      final cbor.ListenerDebug listener = new cbor.ListenerDebug();
+      final cbor.Decoder decoder =
+      new cbor.Decoder.withListener(input, listener);
+      decoder.run();
+    });
+  });
+}
--- test/cbor_decoder_test.dart
+++ test/cbor_decoder_test.dart
@@ -10,32 +10,6 @@ import 'package:test/test.dart';
 import 'cbor_test_listener.dart';
 
 void main() {
-  group('Original C++ tests', () {
-    test('Encode/Decode confidence -> ', () {
-      // Encoding
-      final cbor.OutputDynamic output = new cbor.OutputDynamic();
-      final cbor.Encoder encoder = new cbor.Encoder(output);
-      encoder.writeArray(9);
-      encoder.writeInt(123);
-      encoder.writeInt(-457);
-      encoder.writeString("barrr");
-      encoder.writeInt(321);
-      encoder.writeInt(322);
-      encoder.writeString("foo");
-      encoder.writeBool(true);
-      encoder.writeBool(false);
-      encoder.writeNull();
-      encoder.writeUndefined();
-
-      // Decoding
-      final cbor.Input input = new cbor.Input(output.getData(), output.size());
-      final cbor.ListenerDebug listener = new cbor.ListenerDebug();
-      final cbor.Decoder decoder =
-      new cbor.Decoder.withListener(input, listener);
-      decoder.run();
-    });
-  });
-
   group('RFC Appendix A Diagnostics decoder tests -> ', () {
     // Common initialisation
     final cbor.OutputDynamic output = new cbor.OutputDynamic();
@@ -1691,6 +1665,5 @@ void main() {
       expect(listener.lastValue, ["Fun", true, "Amt", -2]);
       expect(listener.indefinateStart, isTrue);
     });
-
   });
 }
