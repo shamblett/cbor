7619c34242de07c0b0a96a6447af93ae37b726c7
Steve Hamblett <steve.hamblett@linux.com>
2017-03-31 09:54:45 +0000

    CBOR main API and tests

--- lib/src/cbor.dart
+++ lib/src/cbor.dart
@@ -8,4 +8,64 @@
 part of cbor;
 
 /// The CBOR package main API.
-class Cbor {}
+class Cbor {
+
+  /// Decoder
+  typed.Uint8Buffer _buffer;
+  Input _input;
+  Decoder _decoder;
+  Listener _listener;
+
+  Input get input => _input;
+
+  Decoder get decoder => _decoder;
+
+  Output get output => _output;
+
+  typed.Uint8Buffer get buffer => _buffer;
+
+  Listener get listener => _listener;
+
+  set listener(Listener value) {
+    _listener = value;
+  }
+
+  /// Decode from a byte buffer payload
+  void decodeFromBuffer(typed.Uint8Buffer buffer) {
+
+  }
+
+  /// Decode from a list of integer payload
+  void decodeFromList(List<int> ints) {
+
+  }
+
+  /// Get the decoded data as a list
+  List<dynamic> getDecodedData() {
+
+  }
+
+  /// Get the decoded hints
+  List<dataHints> getDecodedHints() {
+
+  }
+
+  /// Pretty print the decoded data
+  String decodedPrettyPrint() {
+
+  }
+
+  /// To JSON
+  String decodedToJSON() {
+
+  }
+
+  /// Encoder
+  Output _output;
+  Encoder _encoder;
+
+  Output get output => _output;
+
+  Encoder get encoder => _encoder;
+
+}
---
+++ test/cbor_api_test.dart
@@ -0,0 +1,22 @@
+/*
+ * Package : Cbor
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 12/12/2016
+ * Copyright :  S.Hamblett
+ */
+import 'package:cbor/cbor.dart' as cbor;
+import 'package:test/test.dart';
+import 'package:typed_data/typed_data.dart' as typed;
+import 'dart:io';
+
+void main() {
+  // Common
+  final cbor.Cbor inst = new cbor.Cbor();
+
+  group("Decoder", () {
+    test('From buffer -> ', () {
+
+
+    });
+  });
+}
