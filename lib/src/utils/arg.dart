9b92406e24a4fe6f6798f07491ddb51026cf1a13
Alex Dochioiu <alex@dochioiu.com>
2024-07-10 13:52:42 +0300

    Fixed negative small int CBOR de-serializing

--- lib/src/utils/arg.dart
+++ lib/src/utils/arg.dart
@@ -45,7 +45,7 @@ class _ArgInt implements Arg {
   final int value;
 
   @override
-  _ArgInt operator ~() => _ArgInt((~value).toSigned(32));
+  _ArgInt operator ~() => _ArgInt((~value).toSigned(33));
 
   @override
   final bool isIndefiniteLength = false;
