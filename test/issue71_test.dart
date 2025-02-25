f7741f5545f4a928f07b640114a75f297d5a8cd7
Steve Hamblett <steve.hamblett@linux.com>
2025-02-25 11:10:56 +0000

    Release 6.3.5

---
+++ test/issue71_test.dart
@@ -0,0 +1,14 @@
+/*
+ * Package : Cbor
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 02/04/2020
+ * Copyright :  S.Hamblett
+ */
+import 'package:convert/convert.dart';
+import 'package:cbor/cbor.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Decode > 65535 input ints', () {
+  });
+}
