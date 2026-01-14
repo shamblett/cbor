5d27f329c874b0d151ddf68173f237717278fb9e
Steve Hamblett <steve.hamblett@linux.com>
2026-01-14 20:24:03 +0000

    Issue 80

---
+++ test/issues/issue77.dart
@@ -0,0 +1,21 @@
+/*
+ * Package : Cbor
+ * Author : A. Dochioiu <alex@vespr.xyz>
+ * Date   : 11/04/2023
+ * Copyright :  A.Dochioiu
+ */
+import 'package:cbor/cbor.dart';
+import 'package:hex/hex.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test(
+    'CBOR List missing tags when decoded',
+        () {
+      const original = "81D8405820E8824C7A0C4E4D7D58009BA1675A72656F4899278906D9523CF0408F1A98F7FB";
+      const originalList = '[$original]';
+      final result = cborDecode(HEX.decode(original));
+      expect(result.toJson().toString() == originalList, isFalse);
+    },
+  );
+}
